a52db44f4a18cbad1767d37ea4e12ecba21497f7
RolandStrod <KhaleAldeAsad@gmail.com>
2021-01-14 13:05:02 +0200

    Patch

---
+++ SnakeGame/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace SnakeGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int score = 0;
+            Walls walls = new Walls(119, 29);
+            walls.Draw();
+            Point snakeTail = new Point(5, 5, 's');
+            Snake snake = new Snake(snakeTail, 10, Direction.RIGHT);
+            snake.Draw();
+            FoodGenerator foodGenerator = new FoodGenerator(119, 29, '$');
+            Point food = foodGenerator.GenerateFood();
+            food.Draw();
+
+
+            while (true)
+            {
+                if(walls.IsHit(snake) || snake.IsHitTail())
+                {
+                    break;
+                }
+                if (snake.Eat(food))
+                {
+                    food = foodGenerator.GenerateFood();
+                    food.Draw();
+                   
+
+                    Random rnd = new Random();
+                    int num = rnd.Next(1, 3);
+                    if(num == 1)
+                    {
+                        score = score + 2;
+                    }
+                    else
+                    {
+                        score--;
+                    }
+                }
+                else
+                {
+                    snake.Move();
+                }
+
+
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey();
+                    snake.HandleKeys(key.Key);
+                }
+
+                Thread.Sleep(300);
+               
+            }
+
+            string str_score = Convert.ToString(score);
+            WriteGameOver(str_score);
+            Console.ReadLine();
+
+
+
+            
+
+        }
+
+        public static void WriteGameOver(string score)
+        {
+            int xOffset = 25;
+            int yOffset = 8;
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.SetCursorPosition(xOffset, yOffset++);
+            WriteText("========================", xOffset, yOffset++);
+            WriteText("        GAME OVER        ", xOffset+1, yOffset++);
+            yOffset++;
+            WriteText($"You scored {score} points", xOffset+2, yOffset++);
+            WriteText("", xOffset+1, yOffset++);
+            WriteText("========================", xOffset, yOffset++);
+        }
+          
+       public static void WriteText(String text, int xOffset,int yOffset)
+        {
+            Console.SetCursorPosition(xOffset, yOffset);
+            Console.WriteLine(text);
+        }
+    }
+}
---
+++ SnakeGame/Walls.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnakeGame
+{
+    class Walls
+    {
+        List<Figure> wallList;
+
+        public Walls(int mapWidth, int mapHeight)
+        {
+            wallList = new List<Figure>();
+            HorizontalLine top = new HorizontalLine(0, 119, 0, '#');
+            VerticalLine left = new VerticalLine(0, 29, 0, '#');
+            HorizontalLine bottom = new HorizontalLine(0, 119, 29, '#');
+            VerticalLine right = new VerticalLine(0, 29, 119, '#');
+
+            VerticalLine obstacle = new VerticalLine(10, 13, 50, 'W');
+            HorizontalLine obstacle2 = new HorizontalLine(2, 12, 21, 'W');
+            wallList.Add(top);
+            wallList.Add(left);
+            wallList.Add(bottom);
+            wallList.Add(right);
+            wallList.Add(obstacle);
+            wallList.Add(obstacle2);
+            
+
+           
+        }
+
+        public void Draw()
+        {
+            foreach(var wall in wallList)
+            {
+                wall.Draw();
+            }
+        }
+
+        public bool IsHit(Figure figure)
+        {
+            foreach(var wall in wallList)
+            {
+                if (wall.IsHit(figure))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
